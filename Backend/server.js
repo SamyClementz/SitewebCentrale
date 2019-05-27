67b5116060d9d79c3a248fb4e9dfeda645026b2e
SamyC <samy.clementz@u-psud.fr>
2019-05-27 13:31:13 +0200

    Création serveur express

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,11 @@
 const express = require('express')
 const app = express()
+var FileStore = require('express-file-store');
+ 
+// using file system
+var fileStore = FileStore('fs', {
+  path: __dirname + '/MachineLearning'
+});
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
