f942259ea2da4356f219d0a4b60b9322b32e1f1b
SamyC <samy.clementz@u-psud.fr>
2019-05-29 13:15:27 +0200

    Création branche dev (passer de Iris à Airbnb)

--- Frontend/src/app/Iris.ts
+++ Frontend/src/app/Iris.ts
@@ -1,6 +1,19 @@
-export class Iris {
-    sepalLength: number = 1;
-    sepalWidth: number = 1;
-    petalLength: number = 1;
-    petalWidth: number = 1; 
+export class Iris {    
+    host_listings_count: number = 1;
+    accommodates: number = 1;
+    bathrooms: number = 1;
+    bedrooms: number = 1;
+    beds: number = 1;
+    number_of_reviews: number = 1;
+    metres: number = 1;
+
+    neighbourhood_cleansed: string;
+
+    property_type: string;
+
+    room_type: string;
+
+    bed_type: string;
+    
+    amenities: string[];
 }
